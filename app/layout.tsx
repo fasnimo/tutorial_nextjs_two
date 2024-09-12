e668e2b4ce43d48856b252635a75179684a69143
fasnimo <fasnimo@gmail.com>
2024-09-11 21:29:59 -0500

    adds font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,19 @@
 import './globals.css';
 import NavBar from '@/components/NavBar';
+import {Inter, Inconsolata, Roboto} from 'next/font/google';
+
+const inter = Inter({subsets:['latin']});
+// const inconsolata = Inconsolata({subsets:['latin']});
+// const roboto = Roboto({subsets:['latin'], weight:['400']});
 
 export default function RootLayout({children}:{children:React.ReactNode}){
-  return <html lang='en'>
-    <body>
-      <NavBar/>
-      <main className='max-w-3xl mx-auto py-10'>{children}</main>
-    </body>
-  </html>
-
+  return ( 
+    <html lang='en'>
+      <body className={inter.className}>
+        <NavBar />
+        <main className='max-w-3xl mx-auto py-10'>{children}</main>
+      </body>
+    </html>
+  )
   
 }
