efdf0ebf0555083f4bba4dceb53ef261c004097c
fasnimo <fasnimo@gmail.com>
2024-11-11 18:02:26 -0600

    adds sign-in functionality

---
+++ app/(dashboard)/auth/[sign-in]/page.tsx
@@ -0,0 +1,6 @@
+const SignInPage = ({ params }: {params: { 'sign-in': string } }) => {
+    console.log(params);
+    return <div>SignInPage</div>
+}
+
+export default SignInPage
---
+++ app/(dashboard)/auth/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AuthPage() {
+  return (
+    <div>
+        <h1 className='text-3xl'>AuthPage</h1>
+    </div>
+  )
+}
